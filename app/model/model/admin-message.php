e65ce64016e18f16966de81e2b854a39c92c25b5
studentdata123 <81951532+studentdata123@users.noreply.github.com>
2021-04-06 09:25:06 +0530

    Delete app/model/model directory

--- app/model/model/admin-message.php
+++
@@ -1,9 +0,0 @@
-<?php
-$msg_valid = 'Email id OR password should be valid !';
-$img_ext_err = 'Image should be in "jpg", "jpeg", "gif" or "png" format.';
-$vdo_ext_err = 'video should be in "wmv", "avi" or "mp4" format.';
-$mp3_ext_err = 'Music should be in Mp3 format.';
-
-// Messages regarding Registration
-$msg_insert_register = 'You have successfully registered. Please <a target="_blank" href="printform.php?pdfid='.isset($_GET['pdfid']).'">click</a> here to download PDF';
-?>
--- app/model/model/table_structure.php
+++
@@ -1,5 +0,0 @@
-<?php
-$tbl_user = "tbl_user";
-$tbl_student = "tbl_student";
-$tbl_studentcampous = "tbl_studentcampous";
-?>
