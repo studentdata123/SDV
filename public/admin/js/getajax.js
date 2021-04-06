d5af3714211988960d800735b21d2ccc55991ae8
studentdata123 <81951532+studentdata123@users.noreply.github.com>
2021-04-06 09:30:07 +0530

    Add files via upload

---
+++ public/admin/js/getajax.js
@@ -0,0 +1,115 @@
+
+
+$('#search_id').click(function(){
+
+  var emp_id = $('#empsearch_text').val().trim();
+
+  //alert(emp_id);
+
+  $.ajax({
+            url: 'getsearchuser.php',
+            type: 'GET',
+            data: {
+                emp_id: emp_id
+            },
+            success: function(returnData){
+                //Blank the search_results div.
+                //$('#user_id').html('');
+                //Parse the result that we got back from search.php
+                var results = JSON.parse(returnData);
+                //alert(results);
+                //var user_id = $('#user_id').val(returnData.user_id);
+                //var user_id = val(returnData.user_id);
+                //alert(user_id);
+                //$('#user_name').val(results.user_name);
+                //Loop through the results array and append it to
+                //the search_results div.
+                // $.each(results, function(key, value){
+                //     $('#user_id').append(value + '<br>');
+                // });
+                //If the results array is empty, display a
+                //message saying that no results were found.
+               var mnguser =  results.manage_user;
+
+var markup = "<label for='inputEmail3' class='col-sm-2 col-form-label'>Employee Id</label><div class='col-sm-4' ><input name='user_id' id='user_id' class='form-control' value='"+ results.user_id +"' type='text'></div><label for='inputEmail3' class='col-sm-2 col-form-label'>Employee Name</label><div class='col-sm-4' ><input name='user_name' id='user_name' class='form-control' value='"+ results.user_name +"' type='text'></div>";
+                //var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
+                $('#valueput').append(markup);
+                $('#valueput').css("display", "show");
+                //$('#search_id').css("pointer-events", "none");
+                $('#search_id').attr('disabled','disabled');
+
+                if(mnguser == 1){
+                 $('#manage_user').attr('checked','checked');
+                }
+                if(results.upload_software == 1){
+                 $('#upload_software').attr('checked','checked');
+                }
+                if(results.manage_csp == 1){
+                 $('#manage_csp').attr('checked','checked');
+                }
+                if(results.upload_sbicomm == 1){
+                 $('#upload_sbicomm').attr('checked','checked');
+                }
+                if(results.manage_notification == 1){
+                 $('#manage_notification').attr('checked','checked');
+                }
+                if(results.upload_agreement == 1){
+                 $('#upload_agreement').attr('checked','checked');
+                }
+				        if(results.manage_visitors == 1){
+                 $('#manage_visitors').attr('checked','checked');
+                }
+                if(results.dm_module == 1){
+                 $('#dm_module').attr('checked','checked');
+                }
+                if(results.adhar_managemant == 1){
+                 $('#adhar_managemant').attr('checked','checked');
+                }
+            
+
+                if(results == 0){
+                    $('#valueput').html('No results found!');
+                    $('#valueput').css("text-align", "center");
+                    //$('#valueput').css("display", "none");
+                }
+            }
+        });
+});
+
+
+$('#manage_user').on('change', function(){
+   $('#hdmanage_user').val(this.checked ? 1 : 0);
+});
+
+ $(document).ready(function() { 
+   $('#mobile_no').on('blur', function(){
+	  var mobile = $('#mobile_no').val();
+	  if (mobile == '') {
+		mobile_state = false;
+		return;
+	  }
+	  $.ajax({
+		  url: 'checkmobile',
+		  type: 'post',
+		  data: {
+			'mobile_check' : 1,
+			'mobile' : mobile,
+		  },
+		  success: function(response){
+			if (response == 'taken' ) {
+			  mobile_state = false;
+			  $('#mobile_no').css({"borderColor":"red"});
+			  $("#mobile_exist").html("<font color='red'>Mobile Number already exist!</font>");
+			  $("#mobile_no").reset();
+			}
+
+			else if (response == 'not_taken') {
+			   mobile_state = true;
+			   $('#mobile_no').css({"borderColor":""});
+			   $("#mobile_exist").html("");
+			 }
+		  }
+	  });
+	 });
+	
+});
---
+++ public/admin/js/validation.js
@@ -0,0 +1,168 @@
+
+// for  COMMISSION submit confirm //
+
+function commsubval()
+{
+	
+  if($("#sel_year").val()=="")
+    {
+        $('#sel_year').css({"borderColor":"red"});
+        $('#sel_year').focus();
+        $("#year_error").html("<font color='red'>Year should not be blank!</font>");
+        return false;    
+    }else
+    {
+        $('#sel_year').css({"borderColor":""});
+        $("#year_error").html("");
+    }
+
+  if($("#sel_month").val()=="")
+    {
+        $('#sel_month').css({"borderColor":"red"});
+        $('#sel_month').focus();
+        $("#month_error").html("<font color='red'>Month should not be blank!</font>");
+        return false;    
+    }else
+    {
+        $('#sel_month').css({"borderColor":""});
+        $("#month_error").html("");
+    }
+
+       if($("#sbicom_csv").val()=="")
+       {
+         $('#sbicom_csv').css({"borderColor":"red"});
+         $('#sbicom_csv').focus();
+         $("#sbicom_error").html("<font color='red'>Upload field should not be blank!</font>");
+         return false;    
+       }else{
+       	var allowedFiles = [".csv"];
+        var fileUpload = $("#sbicom_csv");
+        var lblError = $("#sbicom_error");
+        var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + allowedFiles.join('|') + ")$");
+       	if(regex.test(fileUpload.val().toLowerCase())) {
+       		$('#sbicom_csv').css({"borderColor":""});
+            $("#sbicom_error").html("");
+       	}
+       	else
+        {
+        $("#sbicom_csv").css("border", "1px solid red");
+        $('#sbicom_csv').focus();
+        $('#sbicom_csv').val('');
+        $("#sbicom_error").html("<font color='red'>Please upload files having extensions .csv</font>");
+        return false;  
+        }
+       }
+
+    if(confirm("Are you confirm to submit")==true)
+        {
+            return true;
+        }else{
+            return false;
+        }
+
+}
+
+function cspattlogsubval()
+{
+  if($("#attd_date").val()=="")
+    {
+        $('#attd_date').css({"borderColor":"red"});
+        $('#attd_date').focus();
+        $("#attd_date_error").html("<font color='red'>Date should not be blank!</font>");
+        return false;    
+    }else
+    {
+        $('#attd_date').css({"borderColor":""});
+        $("#attd_date_error").html("");
+    }
+
+       if($("#sbicom_csv").val()=="")
+       {
+         $('#sbicom_csv').css({"borderColor":"red"});
+         $('#sbicom_csv').focus();
+         $("#sbicom_error").html("<font color='red'>Upload field should not be blank!</font>");
+         return false;    
+       }else{
+        var allowedFiles = [".csv"];
+        var fileUpload = $("#sbicom_csv");
+        var lblError = $("#sbicom_error");
+        var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + allowedFiles.join('|') + ")$");
+        if(regex.test(fileUpload.val().toLowerCase())) {
+          $('#sbicom_csv').css({"borderColor":""});
+            $("#sbicom_error").html("");
+        }
+        else
+        {
+        $("#sbicom_csv").css("border", "1px solid red");
+        $('#sbicom_csv').focus();
+        $('#sbicom_csv').val('');
+        $("#sbicom_error").html("<font color='red'>Please upload files having extensions .csv</font>");
+        return false;  
+        }
+       }
+
+    if(confirm("Are you confirm to submit")==true)
+        {
+            return true;
+        }else{
+            return false;
+        }
+}
+
+
+function newserv(sel){
+    
+  var table_id = "status_"+(sel); 
+
+   var typed_id = $('#'+table_id).val(); 
+
+    if (typed_id == '3')
+    {
+      $(".reason_rejnserv").show();
+    }
+    else
+    {
+      $(".reason_rejnserv").hide();
+    }
+
+    if (typed_id == '5')
+    {
+      $(".reason_forwrd").show();
+    }
+    else
+    {
+      $(".reason_forwrd").hide();
+    }
+    
+}
+
+$(document).ready(function(){
+  
+  $("#user_type").change(function() {
+      var user_type = $('#user_type').val();
+      if (user_type == '6')
+      {
+        $(".showkma_dist").show();
+      }
+      else
+      {
+        $(".showkma_dist").hide();
+      }
+  });
+
+  $("#selectall").click(function(){
+        if(this.checked){
+            $('.checkboxall').each(function(){
+                $(".checkboxall").prop('checked', true);
+            })
+        }else{
+            $('.checkboxall').each(function(){
+                $(".checkboxall").prop('checked', false);
+            })
+        }
+    });
+
+});
+
+
+
